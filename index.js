3fbcb9abbf43aa03e43ebef0aeaf1edd2b364e32
Jordan Cox <jordancox747@outlook.com>
2023-10-15 08:57:09 -0600

    minor refactor of puppeteer function

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // get city, state, zip code, median household income for 2021 and 2000
 // land area
 // latitude and longitude
+// only get cities with populations >= 6000
 
 // const puppeteer = require('puppeteer');
 
 import puppeteer from "puppeteer";
 
-(async () => {
+const runWebScraper = async () => {
   const browser = await puppeteer.launch({ headless: false }); // now you can see the browser running
   const page = await browser.newPage();
 
@@ -19,9 +20,11 @@ import puppeteer from "puppeteer";
   await page.keyboard.press('Enter');
 
   const textSelector = await page.waitForSelector('.city');
-  const fullTitle = await textSelector?.evaluate(element => element.textContent);
+  const fullTitle = await textSelector?.evaluate(element => element.innerHTML);
 
   console.log(fullTitle);
 
   await browser.close();
-})();
+};
+
+runWebScraper();
